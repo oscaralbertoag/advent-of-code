514b8176851eff1caa308ece143a99ef6370d095
Oscar Alvarez <oscar.a.alvarezguevara@ehi.com>
2017-12-04 20:49:57 -0600

    Add Inverse captcha part 2

--- inversecaptcha/InverseCaptcha.py
+++ inversecaptcha/InverseCaptcha.py
@@ -1,9 +1,15 @@
 def main():
+    captcha_choice = input("Captcha 1 or 2?")
     file_path = input("Inverse captcha file path:")
     reverse_captcha_file = open(file_path, "r")
     captcha = reverse_captcha_file.read()
     reverse_captcha_file.close()
-    print("Solution: {}".format(solve_captcha([int(x) for x in captcha])))
+    if captcha_choice == 1:
+        solution = solve_captcha([int(x) for x in captcha])
+    else:
+        solution = solve_captcha_2([int(x) for x in captcha])
+
+    print("Solution: {}".format(solution))
 
 
 def solve_captcha(captcha):
@@ -17,6 +23,24 @@ def solve_captcha(captcha):
             next_digit = captcha[x + 1]
 
         if current_digit == next_digit:
+            captcha_sum += current_digit
+
+    return captcha_sum
+
+
+def solve_captcha_2(captcha):
+    captcha_length = len(captcha)
+    step_size = captcha_length // 2
+    captcha_sum = 0
+    for x in range(0, captcha_length):
+        current_digit = captcha[x]
+        if x + step_size > captcha_length - 1:
+            distance_from_end = captcha_length - 1 - x
+            digit_ahead = captcha[step_size - distance_from_end - 1]  # Subtract one because array is zero-based
+        else:
+            digit_ahead = captcha[x + step_size]
+
+        if current_digit == digit_ahead:
             captcha_sum += current_digit
 
     return captcha_sum
--- inversecaptcha/test_solve_captcha.py
+++ inversecaptcha/test_solve_captcha.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from inversecaptcha.InverseCaptcha import solve_captcha
+from inversecaptcha.InverseCaptcha import solve_captcha, solve_captcha_2
 
 
 class TestSolveCaptcha(TestCase):
@@ -9,3 +9,8 @@ class TestSolveCaptcha(TestCase):
         input_to_expected = {"1122": 3, "1111": 4, "1234": 0, "91212129": 9}
         for test_input, expected_result in input_to_expected.items():
             self.assertEqual(expected_result, solve_captcha([int(x) for x in test_input]))  # List comprehension
+
+    def test_solve_captcha_2(self):
+        input_to_expected = {"1212": 6, "1221": 0, "123425": 4, "123123": 12, "12131415": 4}
+        for test_input, expected_result in input_to_expected.items():
+            self.assertEqual(expected_result, solve_captcha_2([int(x) for x in test_input]))
