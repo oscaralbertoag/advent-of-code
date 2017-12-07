cd9ff161b8ce28ca15f4415f62a2b1dfb0e75b5b
Oscar Alvarez <oscar.a.alvarezguevara@ehi.com>
2017-12-06 22:50:07 -0600

    Add checksum part 2.

--- checksum/Checksum.py
+++ checksum/Checksum.py
@@ -1,11 +1,18 @@
 def main():
-    file_path = input("Enter path containing puzzle input:")
+    file_path = input("Enter path containing puzzle input: ")
+    algorithm_selection = input("Use checksum 1 or 2? ")
     puzzle_input = open(file_path, "r")
     input_lines = puzzle_input.readlines()
     lines_no_whitespace = []
     for line in input_lines:
         lines_no_whitespace.append(line.split())
-    print("Solution: {}".format(calculate_checksum(lines_no_whitespace)))
+
+    if algorithm_selection == 1:
+        solution = calculate_checksum(lines_no_whitespace)
+    else:
+        solution = calculate_checksum_2(lines_no_whitespace)
+
+    print("Solution: {}".format(solution))
 
 
 def calculate_checksum(input_lines):
@@ -16,6 +23,26 @@ def calculate_checksum(input_lines):
         first = sorted_line[0]
         difference = last - first
         checksum += difference if difference > 0 else 0
+    return checksum
+
+
+def calculate_checksum_2(input_lines):
+    checksum = 0
+    for line in input_lines:
+        sorted_line = sorted([int(x) for x in line])
+
+        for x in range(0, len(sorted_line) - 1):
+            found_remainder_zero = False
+            for y in range(x + 1, len(sorted_line)):
+                divisor = sorted_line[x]
+                dividend = sorted_line[y]
+                if dividend % divisor == 0:
+                    checksum += dividend // divisor
+                    found_remainder_zero = True
+                    break
+            if found_remainder_zero:
+                break
+
     return checksum
 
 
--- checksum/test_calculate_checksum.py
+++ checksum/test_calculate_checksum.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from checksum.Checksum import calculate_checksum
+from checksum.Checksum import calculate_checksum, calculate_checksum_2
 
 
 class TestCalculateChecksum(TestCase):
@@ -11,3 +11,10 @@ class TestCalculateChecksum(TestCase):
                         [2, 4, 6, 8]]
         checksum = calculate_checksum(input_matrix)
         self.assertEqual(18, checksum)
+
+    def test_calculate_checksum_2(self):
+        input_matrix = [[5, 9, 2, 8],
+                        [9, 4, 7, 3],
+                        [3, 8, 6, 5]]
+        checksum = calculate_checksum_2(input_matrix)
+        self.assertEqual(9, checksum)
